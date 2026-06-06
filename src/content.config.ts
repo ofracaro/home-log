b72d07177ce5ac2062a31d50d302dd2d8659aa88
ofraca <omar.fracaro@gmail.com>
2026-06-06 01:13:24 -0300

    update content.

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,19 +2,16 @@ import { defineCollection, z } from 'astro:content';
 import { glob } from 'astro/loaders'; // <-- Cambió la procedencia del loader a 'astro/loaders'
 
 const logCollection = defineCollection({
-  // Esta es la sintaxis oficial actual para cargar colecciones de Markdown locales
-  loader: glob({ pattern: '**/[^_]*.md', base: "./src/content/log" }),
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    date: z.coerce.date(),
-    category: z.enum(['audiovisual', 'investigacion', 'magazine', 'producto']),
-    coverUrl: z.string(),
-    rol: z.string().optional(),
-    client: z.string().optional(),
-  }),
+	type: 'content',
+	schema: ({ image }) => z.object({
+		title: z.string(),
+		description: z.string(),
+		date: z.date(),
+		category: z.string(),
+		coverUrl: image(),
+	}),
 });
 
 export const collections = {
-  'log': logCollection,
+	'log': logCollection,
 };
